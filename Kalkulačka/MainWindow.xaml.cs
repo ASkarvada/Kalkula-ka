46d50fd57597f5913845e8bcf6089f821ab26665
skarvada.adam.2017 <skarvada.adam.2017@UC404PC15>
2020-01-27 16:54:58 +0100

    oprava základních funkcí

--- Kalkulačka/MainWindow.xaml.cs
+++ Kalkulačka/MainWindow.xaml.cs
@@ -23,70 +23,89 @@ namespace Kalkulačka
         List<double> cisla = new List<double>();
         
         List<char> znamenka = new List<char>();
-        
-        
+
+        bool Nove = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void PrvniPrepsani(int v)
+        {
+            textBlock.Text = Convert.ToString(v);
+            textBlock_znamenka.Text = "";
+            Nove = false;
+            cisla.Clear();
+            znamenka.Clear();
+        }
+
         private void B_0_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 0;
+            if (Nove) PrvniPrepsani(0);
+            else textBlock.Text += 0;
         }
 
         private void B_1_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 1;
+            if (Nove) PrvniPrepsani(1);
+            else textBlock.Text += 1;
         }
 
         private void B_2_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 2;
+            if (Nove) PrvniPrepsani(2);
+            else textBlock.Text += 2;
         }
 
         private void B_3_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 3;
+            if (Nove) PrvniPrepsani(3);
+            else textBlock.Text += 3;
         }
 
         private void B_4_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 4;
+            if (Nove) PrvniPrepsani(4);
+            else textBlock.Text += 4;
         }
 
         private void B_5_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 5;
+            if (Nove) PrvniPrepsani(5);
+            else textBlock.Text += 5;
         }
 
         private void B_6_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 6;
+            if (Nove) PrvniPrepsani(6);
+            else textBlock.Text += 6;
         }
 
         private void B_7_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 7;
+            if (Nove) PrvniPrepsani(7);
+            else textBlock.Text += 7;
         }
 
         private void B_8_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 8;
+            if (Nove) PrvniPrepsani(8);
+            else textBlock.Text += 8;
         }
 
         private void B_9_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text += 9;
+            if (Nove) PrvniPrepsani(9);
+            else textBlock.Text += 9;
         }
 
         private void B_plus_Click(object sender, RoutedEventArgs e)
         {
-            cisla.Add(Convert.ToDouble(textBlock.Text));
-            znamenka.Add('+');
-            textBlock_znamenka.Text = "+ ";
-            textBlock.Text = "";
+            cisla.Add(Convert.ToDouble(textBlock.Text)); //přidam do listu cisel
+            znamenka.Add('+'); //přidam do listu znamenek
+            textBlock_znamenka.Text = "+ "; //upravim tb
+            textBlock.Text = ""; //upravim 2. tb na nic
         }
 
         private void B_minus_Click(object sender, RoutedEventArgs e)
@@ -118,6 +137,9 @@ namespace Kalkulačka
             cisla.Add(Convert.ToDouble(textBlock.Text));
             double x = cisla[0];
 
+
+           
+
             for (int i = 0; i < znamenka.Count; i++)
             {
                 
@@ -141,6 +163,7 @@ namespace Kalkulačka
 
             textBlock_znamenka.Text = "=";
             textBlock.Text = x.ToString();
+            Nove = true;
         }
 
         private void B_point_Click(object sender, RoutedEventArgs e)
